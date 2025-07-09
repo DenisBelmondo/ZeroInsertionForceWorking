e307a96f2b54fd996fa01c2161b47c4ea0f3bf3c
DenisBelmondo <steakwilsomusic@gmail.com>
2025-07-09 16:55:42 -0600

    add status enum to state class

--- src/Belmondo/ZeroInsertionForce/IState.cs
+++ src/Belmondo/ZeroInsertionForce/IState.cs
@@ -2,6 +2,14 @@ namespace Belmondo.ZeroInsertionForce;
 
 public interface IState
 {
+    public enum Status
+    {
+        None,
+        Running,
+        Succeeded,
+        Failed,
+    }
+
     void Enter();
     void Update(double deltaSeconds);
     void Exit();
